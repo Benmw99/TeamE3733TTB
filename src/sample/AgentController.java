4dcf304bf7458654b08b02d05eed196b94184c7d
Ben Waid <benmw10@gmail.com>
2019-01-28 10:57:50 -0500

    Added search, send, and feedback pages

--- src/sample/AgentController.java
+++ src/sample/AgentController.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Button;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -24,9 +25,35 @@ public class AgentController {
     Button agentButt;
 
     @FXML
+    Button agentSearchButton;
+
+    @FXML
+    Button agentSendToAgentButton;
+
+    @FXML
+    Button agentFeedbackButton;
+
+
+    @FXML
     public void agentProfile(ActionEvent event) throws IOException{
         agentControl.pageSwitch(event, "AgentProfile", agentProfileButt);
     }
+
+    @FXML
+    public void agentSearch(ActionEvent event) throws IOException{
+        agentControl.pageSwitch(event, "AgentSearch.fxml", agentSearchButton);
+    }
+
+    @FXML
+    public void agentSendToAgent(ActionEvent event) throws IOException{
+        agentControl.pageSwitch(event, "AgentSendToAgent.fxml", agentSendToAgentButton);
+    }
+
+    @FXML
+    public void agentFeedback(ActionEvent event) throws IOException{
+        agentControl.pageSwitch(event, "AgentFeedback.fxml", agentFeedbackButton);
+    }
+
     /*
     @FXML
    public void samplesceneswitch(ActionEvent event) throws IOException {
