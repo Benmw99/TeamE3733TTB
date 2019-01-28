7de224ba7a5cc57bcffe5f5fdc3d3d31fe198225
jgold189 <climbjn189@gmail.com>
2019-01-27 21:41:29 -0500

    Added Foreign Keys and all remaining primary keys

--- src/TableBuilder.java
+++ src/TableBuilder.java
@@ -1,9 +1,8 @@
 import java.sql.*;
 
-
 //This class may very well not exist on the final project, I'm really just using this as a place to store all the SQL
 //strings as I write them. We definitely need to write those, lol.
-//Even if this class doesn't exist in the final all these methods and strings will be used
+//Even if this class doesn't exist in the final all these create tables will be
 public class TableBuilder {
     private Connection connection;
     private static final String IMAGESIZE = "1M";
@@ -73,29 +72,28 @@ public class TableBuilder {
             Statement stmt = connection.createStatement();
             stmt.execute(dropString);
         } catch (SQLException e) {}
-        //These might not be in the right order for foreign keys
-        buildForm();
-        System.out.println("Build Form");
-        buildUser();
         System.out.println("Build Users");
-        buildBrewersPermit();
-        System.out.println("Build Permit");
-        buildApproval();
-        System.out.println("Build Approval");
-        buildAddress();
-        System.out.println("Build Address");
-        buildCompany();
-        System.out.println("Build Company");
-        buildOtherInfo();
-        System.out.println("Build Other Info");
-        buildWine();
-        System.out.println("Build Wine");
-        buildLabel();
-        System.out.println("Build Label");
-        buildAgents();
+        buildUsers();
         System.out.println("Build Agents");
-        buildReps();
+        buildAgents();
         System.out.println("Build Reps");
+        buildReps();
+        System.out.println("Build Company");
+        buildCompany();
+        System.out.println("Build Form");
+        buildForm();
+        System.out.println("Build Permit");
+        buildBrewersPermit();
+        System.out.println("Build Approval");
+        buildApproval();
+        System.out.println("Build Address");
+        buildAddress();
+        System.out.println("Build Other Info");
+        buildOtherInfo();
+        System.out.println("Build Wine");
+        buildWine();
+        System.out.println("Build Label");
+        buildLabel();
     }
 
     void sendStatement(String buildString) {
@@ -114,68 +112,65 @@ public class TableBuilder {
                 "Zip_Code VARCHAR(8), " +
                 "isMailing BOOLEAN, " +
                 "City VARCHAR(32), " +
-                "TTB_ID BIGINT, " + //TODO FOREIGNKEY, Primary Key
+                "TTB_ID BIGINT, " +
                 "State VARCHAR(2), " +
-                "Street_Name VARCHAR(32))";
+                "Street_Name VARCHAR(32), " +
+                "ID BIGINT," +
+                "Constraint Address_PK Primary Key (ID), " +
+                "Constraint Address_FK Foreign Key (TTB_ID) References Form(TTB_ID) On Delete Cascade)";
         sendStatement(buildString);
     }
 
     void buildOtherInfo(){
         String buildString = "CREATE TABLE OTHERINFO (" +
-                "TTB_ID BIGINT," + //TODO FOREIGNKEY
+                "TTB_ID BIGINT," +
                 "Text VARCHAR(256), " +
-                "Constraint otherInfoPK Primary Key (TTB_ID))";
+                "Constraint OtherInfoPK Primary Key (TTB_ID), " +
+                "Constraint OtherInfoFK Foreign Key (TTB_ID) References Form(TTB_ID) On Delete Cascade)";
         sendStatement(buildString);
     }
 
     void buildWine(){
         String buildString = "CREATE TABLE WINE (" +
                 "pH REAL," + //Might need to make that bigger to store more precision
-                "TTB_ID BIGINT," + //TODO FOREIGN KEY
+                "TTB_ID BIGINT," +
                 "Grape_Varietals VARCHAR(256)," +
                 "Wine_Appellation VARCHAR(32), " +
-                "Constraint Wine_PK Primary Key (TTB_ID))";
-        sendStatement(buildString);
-    }
-
-    void buildCompany(){
-        String buildString = "CREATE TABLE COMPANY (" +
-                "Company_ID BIGINT," + //TODO Foreign Key/Inheritance
-                "Company_Name VARCHAR(256))";
+                "Constraint Wine_PK Primary Key (TTB_ID), " +
+                "Constraint Wine_FK Foreign Key (TTB_ID) References Form(TTB_ID) On Delete Cascade)";
         sendStatement(buildString);
     }
 
     void buildBrewersPermit(){
         String buildString = "CREATE TABLE BREWERSPERMIT (" +
                 "Brewers_No BIGINT," +
-                "TTB_ID BIGINT," + //TODO FOREIGNKEY
+                "TTB_ID BIGINT," +
                 "isPrimary BOOLEAN, " +
-                "Constraint Brewers_Permit_PK Primary Key (TTB_ID, Brewers_No))";
-        sendStatement(buildString);
-    }
-    void buildApproval(){
-        String buildString = "CREATE TABLE APPROVAL (" +
-                "Approving_Agent VARCHAR(32)," +
-                "TTB_ID BIGINT," + //TODO FOREIGNKEY
-                "Date TIMESTAMP," +
-                "Expiration TIMESTAMP," +
-                "Qualification VARCHAR(256), " +
-                "Constraint Approval_PK Primary Key (TTB_ID))";
+                "Constraint BrewersPermit_PK Primary Key (TTB_ID, Brewers_No), " +
+                "Constraint BrewersPermit_FK Foreign Key (TTB_ID) References Form(TTB_ID) On Delete Cascade)";
         sendStatement(buildString);
     }
 
-    void buildUser(){
-        String buildString = "CREATE TABLE USERS (" +
-                "Login_Name VARCHAR(32), " + //TODO Primary Key
-                "Password VARCHAR(256))";
+    void buildApproval(){
+        String buildString = "CREATE TABLE APPROVAL (" +
+                "Approving_Agent VARCHAR(32)," +
+                "TTB_ID BIGINT," +
+                "Date TIMESTAMP," +
+                "Expiration TIMESTAMP," +
+                "Qualification VARCHAR(256), " +
+                "Constraint Approval_PK Primary Key (TTB_ID), " +
+                "Constraint Approval_FK Foreign Key (TTB_ID) References Form(TTB_ID) On Delete Cascade)";
         sendStatement(buildString);
     }
+
     void buildLabel(){
         String buildString = "CREATE TABLE LABEL (" +
-                "ID BigInt, " +
+                "ID BIGINT, " +
                 "Image blob(" + IMAGESIZE + "), " +
                 "ImageName varchar(64), " +
-                "Constraint labels_PK Primary Key (id))";
+                "TTB_ID BIGINT, " +
+                "Constraint Label_PK Primary Key (id), " +
+                "Constraint Label_FK Foreign Key (TTB_ID) References Form(TTB_ID) On Delete Cascade)";
         sendStatement(buildString);
     }
 
@@ -187,27 +182,47 @@ public class TableBuilder {
                 "Brand_Name VARCHAR(256)," +
                 "Source SMALLINT," +
                 "APPROVE SMALLINT," +
-                "Rep_ID VARCHAR(16)," + //TODO Foreign Key
+                "Rep_ID VARCHAR(16)," +
                 "Email VARCHAR(256)," +
-                "Company_ID BIGINT," + //TODO MAKE A FOREIGN KEY
+                "Company_ID BIGINT," +
                 "Date_Submitted TIMESTAMP," +
                 "Applicant_Name VARCHAR(32)," +
                 "Phone VARCHAR(12)," +
                 "Alcohol_Type SMALLINT," +
-                "Constraint form_PK Primary Key (TTB_ID))";
+                "Constraint Form_PK Primary Key (TTB_ID), " +
+                "Constraint Form_FK_Rep Foreign Key (Rep_ID) References Reps(Rep_ID) On Delete Cascade, " +
+                "Constraint Form_FK_Company Foreign Key (Company_ID) References Company(Company_ID) On Delete Cascade)";
+        sendStatement(buildString);
+    }
+
+    void buildUsers(){
+        String buildString = "CREATE TABLE USERS (" +
+                "Login_Name VARCHAR(32), " +
+                "Password VARCHAR(256), " +
+                "Constraint Users_PK Primary Key (Login_Name))";
         sendStatement(buildString);
     }
 
     void buildAgents() {
         String buildString = "CREATE TABLE AGENTS (" +
                 "Agent_Name VARCHAR(32), " +
-                "Agent_ID BIGINT)"; //TODO Foreign key/inheritance
+                "Agent_ID BIGINT, " + //TODO Inheritance
+                "Constraint Agents_UQ Unique (Agent_ID))";
         sendStatement(buildString);
     }
 
     void buildReps() {
         String buildString = "CREATE TABLE REPS (" +
-                "RepID VARCHAR(16))"; //TODO Foreign key/inheritance
+                "Rep_ID VARCHAR(16), " +//TODO Inheritance
+                "Constraint Reps_UQ Unique (Rep_ID))";
+        sendStatement(buildString);
+    }
+
+    void buildCompany(){
+        String buildString = "CREATE TABLE COMPANY (" +
+                "Company_ID BIGINT," + //TODO Inheritance
+                "Company_Name VARCHAR(256), " +
+                "Constraint Company_UQ Unique (Company_ID))";
         sendStatement(buildString);
     }
 }
