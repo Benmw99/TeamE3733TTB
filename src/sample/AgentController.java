554eb1bcb2df031d5f9c399ca02c2a79001cf2be
Sierra Palmer <stpalmer@wpi.edu>
2019-01-27 22:59:08 -0500

    created agent profile page and controller for agent *still needs functionality*

---
+++ src/sample/AgentController.java
@@ -0,0 +1,31 @@
+package sample;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+
+public class AgentController {
+
+    @FXML
+    Button agentProfileButt;
+
+    @FXML
+    Button agentButt;
+
+    /*
+    @FXML
+   public void samplesceneswitch(ActionEvent event) throws IOException {
+        pageSwitch(event, "filename", buttonName);
+    }
+
+    */
+}
