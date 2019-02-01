b193a058020d8ffb27285ccb0ca983e3b5a1bcb8
Ben W <benmw10@gmail.com>
2019-01-31 21:34:09 -0500

    Added IUser and its sub-classes Agent, Manufacturer, and Representative.

---
+++ src/Entities/Representative.java
@@ -0,0 +1,30 @@
+package Entities;
+
+public class Representative implements IUser {
+
+    String repID;
+
+    @Override
+    public void login() {
+
+    }
+
+    @Override
+    public String search() {
+        return null;
+    }
+
+    void SubmitForm() {
+
+    }
+
+    void SubmitLabel() {
+
+    }
+
+    void CheckProgress() {
+
+    }
+
+
+}
