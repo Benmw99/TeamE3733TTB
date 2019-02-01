6251ed0a1a2ac0fc94e4c68f82ebf90ea6356fe1
neusman1 <nick.eusman@gmail.com>
2019-01-31 22:24:58 -0500

    form constructor

--- src/Entities/Form.java
+++ src/Entities/Form.java
@@ -17,5 +17,19 @@ public class Form {
     private String brewersPermit;
     private Approval approval;
 
-
+    public Form(String fancifulName, String brandName, boolean source, String repID, int ttbID, String email, Timestamp dateSubmitted, String applicantName, String phoneNum, AlcoholType alcoholType, String otherInfo, String brewersPermit, Approval approval) {
+        this.fancifulName = fancifulName;
+        this.brandName = brandName;
+        this.source = source;
+        this.repID = repID;
+        this.ttbID = ttbID;
+        this.email = email;
+        this.dateSubmitted = dateSubmitted;
+        this.applicantName = applicantName;
+        this.phoneNum = phoneNum;
+        this.alcoholType = alcoholType;
+        this.otherInfo = otherInfo;
+        this.brewersPermit = brewersPermit;
+        this.approval = approval;
+    }
 }
