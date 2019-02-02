37e21c40f05ac9e1f1bebb663a30910d555503e5
neusman1 <nick.eusman@gmail.com>
2019-02-02 01:01:36 -0500

    Added sorting! currently has for date and brandname, can implement more as needed. added constructor and getter/setter to approval

--- src/Entities/Approval.java
+++ src/Entities/Approval.java
@@ -11,7 +11,52 @@ public class Approval {
     private Date expDate;
     private String qualifications;
 
+    public Approval(boolean aproved, Date timestamp, Agent agentApproval, Date expDate, String qualifications) {
+        this.aproved = aproved;
+        this.timestamp = timestamp;
+        this.agentApproval = agentApproval;
+        this.expDate = expDate;
+        this.qualifications = qualifications;
+    }
 
+    public boolean isAproved() {
+        return aproved;
+    }
 
+    public void setAproved(boolean aproved) {
+        this.aproved = aproved;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Agent getAgentApproval() {
+        return agentApproval;
+    }
+
+    public void setAgentApproval(Agent agentApproval) {
+        this.agentApproval = agentApproval;
+    }
+
+    public Date getExpDate() {
+        return expDate;
+    }
+
+    public void setExpDate(Date expDate) {
+        this.expDate = expDate;
+    }
+
+    public String getQualifications() {
+        return qualifications;
+    }
+
+    public void setQualifications(String qualifications) {
+        this.qualifications = qualifications;
+    }
 }
 
--- src/Entities/SearchResult.java
+++ src/Entities/SearchResult.java
@@ -1,6 +1,16 @@
 package Entities;
 
+import Entities.Sorting.BrandNameSort;
+import Entities.Sorting.DateSort;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+
 
 public class SearchResult {
     private ArrayList<Form> results;
@@ -36,12 +46,38 @@ public class SearchResult {
         results.addAll(forms);
     }
 
-    public void printResults(){
+    public boolean printResults(){
 
-        for( int i = 1; i < results.size(); i++){
+        try {
+            String home = System.getProperty("user.home");
+            Date date = new Date() ;
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss") ;
+            File file = new File(home + "/Downloads/" + "TTBSearch" +dateFormat.format(date) + ".csv");
+            FileWriter outputfile = new FileWriter(file);
+            //CSVWriter writer = new CSVWriter(outputfile);
+            //String[] header = { "Name", "Class", "Marks" };
+            //writer.writeNext(header);
 
+            for (int i = 1; i < results.size(); i++) {
+
+            }
+
+
+            return true;
+        }catch(IOException e){
+            e.printStackTrace();
+            return false;
         }
-
     }
+
+
+    public void sortBrandName(){
+        Collections.sort(results, new BrandNameSort());
+    }
+
+    public void sortDate(){
+        Collections.sort(results, new DateSort());
+    }
+
 
 }
