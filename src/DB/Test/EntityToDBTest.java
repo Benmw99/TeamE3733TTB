a1ddfb0513ab2fa27c07dfa8a9b38ab31f6fc009
mjclements <mjclements@wpi.edu>
2019-02-04 18:33:14 -0500

    insertForm and getForm actually Work!

--- src/DB/Test/EntityToDBTest.java
+++ src/DB/Test/EntityToDBTest.java
@@ -13,18 +13,21 @@ import static org.junit.Assert.*;
 public class EntityToDBTest {
     Database db = Database.getInstance();
     Form one;
+    int ID_ONE;
     @Before
     public void init()
-
     {
         db.tableBuilder.resetDB();
         one = new Form(AlcoholType.DistilledLiquor,
                 "BUDDY", (float) 5.6);
+        one.setPhoneNumber("6035026034");
+        one.setEmail("Jim@jimmail.com");
         Manufacturer man = new Manufacturer("sysadmin@swollenjams.com", "PASS");
         man.setManID(123);
         try {
             db.dbInsert.insertCompany(123, "Buddweiser", "User", "Pass");
-            db.dbInsert.insertForm(one, man);
+            ID_ONE = db.dbInsert.insertForm(one, man);
+            System.out.println(ID_ONE);
         } catch (SQLException e){
             System.out.println(e.toString());
         }
@@ -32,7 +35,12 @@ public class EntityToDBTest {
 
     @Test
     public void getFormTest(){
-        assertTrue(one.equals(db.dbSelect.getFormByTTB_ID(one.getTtbID())));
+        Form same = db.dbSelect.getFormByTTB_ID(ID_ONE);
+      //  assertEquals(one.getTtbID(), same.getTtbID());
+        assertEquals(one.getEmail(), same.getEmail());
+        assertEquals(one.getFancifulName(), same.getFancifulName());
+        assertEquals(one.getRepID(), same.getRepID());
+        assertEquals(one.getBrandName(), same.getBrandName());
     }
 
 }
