25ef86b144b9fef75bb076844ce6d94c51ae5478
jgold189 <climbjn189@gmail.com>
2019-02-06 02:16:28 -0500

    Pushed DB that was setup already

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,16 @@ import DB.SampleData;
 public class Main {
 
     public static void main(String[] args) {
-        try {
+        //Test Agent login:TestAgent Password:1234
+        //Test Company login:TestCompany Password:1234
+        /*try {
             DB.Database db = DB.Database.getInstance();
             db.tableBuilder.resetDB();
             db.dbInsert.insertAgent("Joe Cool", 445, "TestAgent", "1234");
             DB.SampleData s = new SampleData(db);
         }catch(Exception e){
             System.out.println(e.toString());
-        }
+        }*/
         UI.MainUI.main( args);
     }
 }
