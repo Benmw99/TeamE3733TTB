8b31b8179513ece0beae048516c07ac2f4b63849
jgold189 <climbjn189@gmail.com>
2019-02-05 23:55:14 -0500

    Revamped tests

--- src/UI/CivilController.java
+++ src/UI/CivilController.java
@@ -166,6 +166,9 @@ public class CivilController {
     SearchResult result;
     int searchPage;
 
+    //Brewers Permit
+    //Mailing address
+
     @FXML
     protected void initialize() {
         if (initStuff == 1) {
