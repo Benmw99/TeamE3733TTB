51a2866b189f874a1a9ab98c56ab5fe169872cc5
mjclements <mjclements@wpi.edu>
2019-02-08 19:00:19 -0500

    formDisplay implemented by Agent Controller

--- src/UI/AgentController.java
+++ src/UI/AgentController.java
@@ -37,7 +37,7 @@ import DB.*;
 
 import javax.print.attribute.standard.Media;
 
-public class AgentController {
+public class AgentController implements IFormDisplay {
     //AgentSearch
     @FXML
     Button menuASButton;
@@ -505,55 +505,181 @@ public class AgentController {
 
     //Form Labels
     @FXML
-    Label Agent1Label;
+    Label Display1Label;
     @FXML
-    Label Agent2Label;
+    Label Display2Label;
     @FXML
-    Label Agent3Label;
+    Label Display3Label;
     @FXML
-    Label AgentReview4Label1;
+    Label DisplayReview4Label1;
     @FXML
-    Label Agent4Label2;
+    Label Display4Label2;
     @FXML
-    Label Agent5Label1;
+    Label Display5Label1;
     @FXML
-    Label Agent5Label2;
+    Label Display5Label2;
     @FXML
-    Label Agent5Label3;
+    Label Display5Label3;
     @FXML
-    Label Agent6Label;
+    Label Display6Label;
     @FXML
-    Label Agent7Label;
+    Label Display7Label;
     @FXML
-    Label Agent8Label;
+    Label Display8Label;
     @FXML
-    Label Agent9Label;
+    Label Display9Label;
     @FXML
-    Label Agent10Label;
+    Label Display10Label;
     @FXML
-    Label Agent11Label;
+    Label Display11Label;
     @FXML
-    Label Agent12Label;
+    Label Display12Label;
     @FXML
-    Label Agent13Label;
+    Label Display13Label;
     @FXML
-    Label Agent14Label;
+    Label Display14Label;
     @FXML
-    Label Agent15Label1;
+    Label Display15Label1;
     @FXML
-    Label Agent15Label2;
+    Label Display15Label2;
     @FXML
-    Label Agent15Label3;
+    Label Display15Label3;
     @FXML
-    Label Agent16Label1;
+    Label Display16Label1;
     @FXML
-    Label Agent16Label2;
+    Label Display16Label2;
+
+    @Override
+    public Label getDisplay1Label() {
+        return Display1Label;
+    }
+
+    @Override
+    public Label getDisplay2Label() {
+        return Display2Label;
+    }
+
+    @Override
+    public Label getDisplay3Label() {
+        return Display3Label;
+    }
+
+    @Override
+    public Label getDisplayReview4Label1() {
+        return DisplayReview4Label1;
+    }
+
+    @Override
+    public Label getDisplay4Label2() {
+        return Display4Label2;
+    }
+
+    @Override
+    public Label getDisplay5Label1() {
+        return Display5Label1;
+    }
+
+    @Override
+    public Label getDisplay5Label2() {
+        return Display5Label2;
+    }
+
+    @Override
+    public Label getDisplay5Label3() {
+        return Display5Label3;
+    }
+
+    @Override
+    public Label getDisplay6Label() {
+        return Display6Label;
+    }
+
+    @Override
+    public Label getDisplay7Label() {
+        return Display7Label;
+    }
+
+    @Override
+    public Label getDisplay8Label() {
+        return Display8Label;
+    }
+
+    @Override
+    public Label getDisplay9Label() {
+        return Display9Label;
+    }
+
+    @Override
+    public Label getDisplay10Label() {
+        return Display10Label;
+    }
+
+    @Override
+    public Label getDisplay11Label() {
+        return Display11Label;
+    }
+
+    @Override
+    public Label getDisplay12Label() {
+        return Display12Label;
+    }
+
+    @Override
+    public Label getDisplay13Label() {
+        return Display13Label;
+    }
+
+    @Override
+    public Label getDisplay14Label() {
+        return Display14Label;
+    }
+
+    @Override
+    public Label getDisplay15Label1() {
+        return Display15Label1;
+    }
+
+    @Override
+    public Label getDisplay15Label2() {
+        return Display15Label2;
+    }
+
+    @Override
+    public Label getDisplay15Label3() {
+        return Display15Label3;
+    }
+
+    @Override
+    public Label getDisplay16Label1() {
+        return Display16Label1;
+    }
+
+    @Override
+    public Label getDisplay16Label2() {
+        return Display16Label2;
+    }
+
+    @Override
+    public Label getDisplay17Label() {
+        return Display17Label;
+    }
+
+    @Override
+    public Label getDisplay18Label() {
+        return Display18Label;
+    }
+
+    @Override
+    public Label getDisplay20Label() {
+        return Display20Label;
+    }
+
     @FXML
-    Label Agent17Label;
+    Label Display17Label;
     @FXML
-    Label Agent18Label;
+    Label Display18Label;
     @FXML
-    Label Agent20Label;
+    Label Display20Label;
 
     @FXML
     TableView<Form> formTable;
