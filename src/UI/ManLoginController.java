734cb82a849baac09c24d7508c232be275dc9dcb
Ben W <benmw10@gmail.com>
2019-02-11 17:36:33 -0500

    A couple manufacturer controllers

--- src/UI/ManLoginController.java
+++ src/UI/ManLoginController.java
@@ -1,14 +1,19 @@
 package UI;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
+import java.io.IOException;
+
 public class ManLoginController extends PageControllerUI implements ILogin{
 
+    @FXML
     TextField LoginUserUsernameTextField;
-
+    @FXML
     TextField LoginUserPasswordTextField;
-
+    @FXML
     Button LoginUserLoginButton;
 
     LoginHelper loginHelper;
@@ -32,8 +37,21 @@ public class ManLoginController extends PageControllerUI implements ILogin{
         return LoginUserLoginButton;
     }
 
+    @Override
     protected void onLeave(){}
+    @Override
+    void onLoad(){
+        attributeContainer.currentUser = null;
+    }
 
-    void onLoad(){}
-
+    @FXML
+    public void login(ActionEvent event) throws IOException {
+        if(loginHelper.authenticate()){
+            goToPage("ManHome.fxml");
+        }
+        else {
+            System.out.println("Uh this should be a popup dude, but you logged in wrong");
+            //TODO: make popup warning
+        }
+    }
 }
